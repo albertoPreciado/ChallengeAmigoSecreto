b4f465b13013927f7ea654231db6ad748483da21
Alberto Preciado <alberto.preciado.f@gmail.com>
2025-09-21 22:44:56 -0600

    Agrego cambios en proyecto

--- app.js
+++ app.js
@@ -1,1 +1,54 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = [];
+
+
+const inputAmigo = document.getElementById("amigo");
+const listaAmigos = document.getElementById("listaAmigos");
+const resultado = document.getElementById("resultado");
+
+function agregarAmigo() {
+    let nombreAmigo = inputAmigo.value.trim();
+
+    if (nombreAmigo === "") {
+        alert("Por favor, inserte un nombre");
+        return;
+    }
+
+    if (amigos.includes(nombreAmigo)) {
+        alert("Ese nombre ya está en la lista");
+        inputAmigo.value = "";
+        inputAmigo.focus();
+        return;
+    }
+
+    amigos.push(nombreAmigo);
+    inputAmigo.value = "";
+    inputAmigo.focus();
+    mostrarListaAmigos();
+}
+
+function mostrarListaAmigos() {
+    listaAmigos.innerHTML = "";
+
+    if (amigos.length === 0) {
+        listaAmigos.innerHTML = "<li>No hay amigos agregados</li>";
+        return;
+    }
+
+    for (let amigo of amigos) {
+        let itemLista = document.createElement("li");
+        itemLista.textContent = amigo;
+        listaAmigos.appendChild(itemLista);
+    }
+}
+
+function sortearAmigo() {
+    let cantidadAmigos = amigos.length;
+
+    if (cantidadAmigos === 0) {
+        alert("Por favor, inserte un nombre antes de sortear");
+        return;
+    }
+
+    let indiceAmigo = Math.floor(Math.random() * cantidadAmigos);
+    resultado.innerHTML = `🎉 El Amigo Secreto es: <strong>${amigos[indiceAmigo]}</strong>`;
+}
